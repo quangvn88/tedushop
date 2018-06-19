fd553d3c33ac4b96665b4d726f2bb0fcf340a583
DESKTOP-6KVA8U6\quangvn <quangvn6388@gmail.com>
2018-06-19 14:42:21 +0700

    19/06/2018

---
+++ TeduShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { set; get; }
+        public string CreatedBy { set; get; }
+        public DateTime? UpdatedDate { set; get; }
+        public string UpdatedBy { set; get; }
+    }
+}
